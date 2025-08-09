343a27f33316e3bdc93f34b7a140283fad52ce4e
SikkieNL <SikkieNL@outlook.com>
2025-08-09 01:43:49 +0100

    Update elist.h

--- elist.h
+++ elist.h
@@ -216,7 +216,7 @@ static inline void list_splice_init(struct list_head *list,
  * @head:	the head for your list.
  * @member:	the name of the list_struct within the struct.
  */
-#define list_for_each_entry(pos, head, member)				\
+#define list_for_each_entry(pos, head, member)			\
 	for (pos = list_entry((head)->next, typeof(*pos), member);	\
 	     &pos->member != (head); 					\
 	     pos = list_entry(pos->member.next, typeof(*pos), member))
@@ -228,7 +228,7 @@ static inline void list_splice_init(struct list_head *list,
  * @head:	the head for your list.
  * @member:	the name of the list_struct within the struct.
  */
-#define list_for_each_entry_safe(pos, n, head, member)			\
+#define list_for_each_entry_safe(pos, n, head, member)		\
 	for (pos = list_entry((head)->next, typeof(*pos), member),	\
 		n = list_entry(pos->member.next, typeof(*pos), member);	\
 	     &pos->member != (head); 					\
@@ -241,7 +241,7 @@ static inline void list_splice_init(struct list_head *list,
  * @head:       the head for your list.
  * @member:     the name of the list_struct within the struct.
  */
-#define list_for_each_entry_continue(pos, head, member)			\
+#define list_for_each_entry_continue(pos, head, member)		\
 	for (pos = list_entry(pos->member.next, typeof(*pos), member),	\
 		     prefetch(pos->member.next);			\
 	     &pos->member != (head);					\
